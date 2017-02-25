6f2552e0cfc25f322120b72635a2ea7664ce37e9
Anatoli Zbirouski <armagast@gmail.com>
2017-02-25 11:18:04 +0300

    Added glusterfs server installation

---
+++ docker-swarm/addhosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cat >> /etc/hosts << EOF
+10.0.0.11 node1
+10.0.0.12 node2
+10.0.0.13 node3
+EOF
--- docker-swarm/installdocker.sh
+++ docker-swarm/installdocker.sh
@@ -2,10 +2,9 @@
 # Docker installation
 
 # Install prerequisites
-apt-get update && apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends \
     linux-image-extra-$(uname -r) \
     linux-image-extra-virtual curl
-# Fetch docker repo gpg key
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 # Add docker repository key
 apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
@@ -15,5 +14,9 @@ add-apt-repository \
        ubuntu-$(lsb_release -cs) \
        main"
 # Install docker
-apt-get update && apt-get -y install docker-engine
+apt-get update && apt-get -y install docker-engine glusterfs-server
+# Start glusterfs server
+service glusterfs-server start
+# Create directories to store data
+mkdir -p /gluster/data /swarm/volumes
 
